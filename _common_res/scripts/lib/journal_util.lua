69bafa838a590cd6f59d82187e891b0e9b0aa1ed
phobos2077 <phobos2077@gmail.com>
2022-04-29 22:55:33 +0300

    Add comment and license to all scripts

--- _common_res/scripts/lib/journal_util.lua
+++ _common_res/scripts/lib/journal_util.lua
@@ -1,3 +1,18 @@
+--[[
+Journal (Book) Utilities
+Version: 1.0
+
+Copyright (c)  2022  phobos2077  (https://steamcommunity.com/id/phobos2077)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including the right to distribute and without limitation the rights to use, copy and/or modify
+the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+--]]
+
 local journal_util = {}
 
 journal_util.Enum = {
--- costly-infrastructure/res/scripts/costly_infrastructure/entity_info.lua
+++ costly-infrastructure/res/scripts/costly_infrastructure/entity_info.lua
@@ -1,3 +1,18 @@
+--[[
+Entity info gathering utilities.
+Version: 1.0
+
+Copyright (c)  2022  phobos2077  (https://steamcommunity.com/id/phobos2077)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including the right to distribute and without limitation the rights to use, copy and/or modify
+the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+--]]
+
 local table_util = require 'lib/table_util'
 local config = require 'costly_infrastructure/config'
 
